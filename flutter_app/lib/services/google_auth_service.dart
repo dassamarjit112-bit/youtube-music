ecf68d4ca5ba79819778a85ea514503bd1d9d11a
dassamarjit112-bit <dassamarjit112@gmail.com>
2026-03-29 02:16:39 +0530

    jjj

--- flutter_app/lib/services/google_auth_service.dart
+++ flutter_app/lib/services/google_auth_service.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:supabase_flutter/supabase_flutter.dart'; // Ensure this is imported
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class GoogleAuthService {
   final GoogleSignIn _googleSignIn = GoogleSignIn(
     serverClientId: '79361906244-0umpdsl6bk6grunhuotbe5qhhb2911uf.apps.googleusercontent.com',
   );
 
-// lib/services/google_auth_service.dart
-Future<Map<String, dynamic>?> handleSignIn() async {
-  try {
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-    if (googleUser != null) {
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      
-      // Sync with Supabase
-      final AuthResponse res = await Supabase.instance.client.auth.signInWithIdToken(
-        provider: OAuthProvider.google,
-        idToken: googleAuth.idToken!,
-        accessToken: googleAuth.accessToken,
-      );
+  Future<Map<String, dynamic>?> handleSignIn() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+        
+        // Sync with Supabase
+        final AuthResponse res = await Supabase.instance.client.auth.signInWithIdToken(
+          provider: OAuthProvider.google,
+          idToken: googleAuth.idToken!,
+          accessToken: googleAuth.accessToken,
+        );
 
-      // Return the user details to main.dart
-      return {
-        "id": res.user?.id,
-        "email": res.user?.email,
-        "full_name": googleUser.displayName,
-        "avatar_url": googleUser.photoUrl,
-      };
+        // Return the user details to main.dart
+        return {
+          "id": res.user?.id,
+          "email": res.user?.email,
+          "full_name": googleUser.displayName,
+          "avatar_url": googleUser.photoUrl,
+        };
+      }
+    } catch (error) {
+      debugPrint("Login Failed: $error");
     }
-  } catch (error) {
-    debugPrint("Login Failed: $error");
+    return null;
   }
-  return null;
 }
-  
